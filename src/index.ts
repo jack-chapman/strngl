63b83f77c648aba8ca906e693c0bf666c132cdc1
jack-chapman <jackchapman093@gmail.com>
2020-06-02 21:42:16 +0100

    Add startsWith to index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { validate } from './validate/index'
 import { required } from './required/index'
+import { startsWith } from './startsWith/index'
 
 export {
   validate,
-  required
+  required,
+  startsWith
 }
