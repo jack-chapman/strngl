5b6cb8a4163197c5d5bf6b799225dd32e7d08370
jack-chapman <jackchapman093@gmail.com>
2020-06-04 09:22:28 +0100

    Add hasMinMax to main export

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { validate } from './validate/index'
 import { required } from './required/index'
 import { startsWithChar } from './startsWithChar/index'
 import { endsWithChar } from './endsWithChar/index'
+import { hasMinMax } from './hasMinMax/index'
 
 export {
   validate,
   required,
   startsWithChar,
-  endsWithChar
+  endsWithChar,
+  hasMinMax
 }
